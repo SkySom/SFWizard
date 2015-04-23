9037b32d991d628edfc527f6054e71eb3bc2ec0b
SkySom <skylar.som@gmail.com>
2015-04-22 22:26:44 -0400

    Made mainpage.fxml load again

--- src/main/java/io/sommers/sfwizard/ui/main/Main.java
+++ src/main/java/io/sommers/sfwizard/ui/main/Main.java
@@ -10,7 +10,7 @@ public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("mainpage.fxml"));
+        Parent root = FXMLLoader.load(getClass().getClassLoader().getResource("mainpage.fxml"));
         primaryStage.setTitle("Hello World");
         primaryStage.setScene(new Scene(root, 300, 275));
         primaryStage.show();
