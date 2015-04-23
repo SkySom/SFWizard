d51abdb5786ca8006bb17b6c10f498e0469b980c
SkySom <skylar.som@gmail.com>
2015-04-23 02:31:11 -0400

    All file choosers and elements in place

--- src/main/java/io/sommers/sfwizard/ui/xmltranslator/XMLTranslatorController.java
+++ src/main/java/io/sommers/sfwizard/ui/xmltranslator/XMLTranslatorController.java
@@ -5,7 +5,11 @@ import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+import javafx.stage.DirectoryChooser;
 import javafx.stage.FileChooser;
+
+import java.io.File;
 
 
 /**
@@ -14,13 +18,43 @@ import javafx.stage.FileChooser;
 public class XMLTranslatorController extends BaseController {
 	@FXML
 	public Button inputFileChooserButton;
+	public Button translationFileChooserButton;
+	public Button outputFileChooserButton;
+	public TextField inputFileChooserTextField;
+	public TextField translationFileChooserTextField;
+	public TextField outputFileChooserTextField;
+
+	private FileChooser fileChooser = new FileChooser();
+	private DirectoryChooser directoryChooser = new DirectoryChooser();
 
 	public void initialize() {
 		inputFileChooserButton.setOnAction(new EventHandler<ActionEvent>() {
 			@Override
 			public void handle(ActionEvent actionEvent) {
-				FileChooser fileChooser = new FileChooser();
-				fileChooser.showOpenDialog(getStage());
+				File file = fileChooser.showOpenDialog(getStage());
+				if(file != null) {
+					inputFileChooserTextField.setText(file.getAbsolutePath());
+				}
+			}
+		});
+
+		translationFileChooserButton.setOnAction(new EventHandler<ActionEvent>() {
+			@Override
+			public void handle(ActionEvent actionEvent) {
+				File file = fileChooser.showOpenDialog(getStage());
+				if(file != null) {
+					translationFileChooserTextField.setText(file.getAbsolutePath());
+				}
+			}
+		});
+
+		outputFileChooserButton.setOnAction(new EventHandler<ActionEvent>() {
+			@Override
+			public void handle(ActionEvent actionEvent) {
+				File file = directoryChooser.showDialog(getStage());
+				if(file != null) {
+					outputFileChooserTextField.setText(file.getAbsolutePath());
+				}
 			}
 		});
 	}
