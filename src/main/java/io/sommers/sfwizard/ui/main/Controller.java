5a291f053611cf3460c7ef3e60b9e6a74f7317f4
SkySom <skylar.som@gmail.com>
2015-04-23 00:30:23 -0400

    Sub panels starting.

--- src/main/java/io/sommers/sfwizard/ui/main/Controller.java
+++ src/main/java/io/sommers/sfwizard/ui/main/Controller.java
@@ -1,4 +1,31 @@
 package io.sommers.sfwizard.ui.main;
 
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.control.Button;
+import javafx.scene.layout.Pane;
+
+import java.io.IOException;
+
 public class Controller {
+	@FXML
+	public Button xmlTranslatorButton;
+	public Pane wizardPane;
+
+	public void initialize() {
+		xmlTranslatorButton.setOnAction(new EventHandler<ActionEvent>() {
+			@Override
+			public void handle(ActionEvent actionEvent) {
+				try {
+					Parent root = FXMLLoader.load(getClass().getClassLoader().getResource("xmltranslation.fxml"));
+					wizardPane.getChildren().add(root);
+				} catch (IOException ioException) {
+					System.out.println(ioException.getStackTrace());
+				}
+			}
+		});
+	}
 }
--- src/main/java/io/sommers/sfwizard/ui/main/Main.java
+++ src/main/java/io/sommers/sfwizard/ui/main/Main.java
@@ -11,8 +11,8 @@ public class Main extends Application {
     @Override
     public void start(Stage primaryStage) throws Exception{
         Parent root = FXMLLoader.load(getClass().getClassLoader().getResource("mainpage.fxml"));
-        primaryStage.setTitle("Hello World");
-        primaryStage.setScene(new Scene(root, 300, 275));
+        primaryStage.setTitle("SFWizard");
+        primaryStage.setScene(new Scene(root, 500, 300));
         primaryStage.show();
     }
 
