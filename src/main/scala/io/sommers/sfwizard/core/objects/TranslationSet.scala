6765d41d87aeb61d54e24209344dc38c674f4dfc
SkySom <skylar.som@gmail.com>
2015-04-28 21:15:51 -0400

    I guess I missed one

---
+++ src/main/scala/io/sommers/sfwizard/core/objects/TranslationSet.scala
@@ -0,0 +1,10 @@
+package io.sommers.sfwizard.core.objects
+
+import info.folone.scala.poi.Row
+
+/**
+ * Created by Skylar on 4/28/2015.
+ */
+class TranslationSet(internalName: String, translations: Set[Translation]) {
+
+}
